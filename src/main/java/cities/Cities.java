aaf32420d4e6a3b379669e5572f5c18f81316319
Diana Paievska <71642076+paievska@users.noreply.github.com>
2023-07-08 21:59:15 +0300

    shuffle list and delete eng part

--- src/main/java/cities/Cities.java
+++ src/main/java/cities/Cities.java
@@ -7,6 +7,7 @@ import org.jsoup.select.Elements;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
@@ -27,15 +28,17 @@ public class Cities {
             for (Element row : rows) {
                 Element secondColumn = row.select("td:eq(1)").first();
                 String lastCity = Objects.requireNonNull(secondColumn).text();
-                cityList.add(lastCity);
+                int index = lastCity.indexOf('(');
+                String ukrCity = index != -1 ? lastCity.substring(0, index).trim() : lastCity;
+                cityList.add(ukrCity);
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-
     public String findCity(char firstChar) {
+        Collections.shuffle(cityList);
         for (int i = 0; i < cityList.size(); i++) {
             String city = cityList.get(i);
             if (Character.toLowerCase(city.charAt(0)) == Character.toLowerCase(firstChar)) {
@@ -46,7 +49,6 @@ public class Cities {
         }
         return null;
     }
-
 
     public String getLastCity() {
         return lastCity;
